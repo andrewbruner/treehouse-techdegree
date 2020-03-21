c5048e5020e570f1eff73a821d7afc271d712268
Andrew Bruner <andrewbruner@gmail.com>
2020-03-21 18:30:34 -0400

    Begin work on showPage and appendPageLinks functions

--- js/script.js
+++ js/script.js
@@ -17,8 +17,8 @@ FSJS project 2 - List Filter and Pagination
    scoped to that function.
 ***/
 
-
-
+const studentList = document.querySelectorAll('.student-item');
+const numberPerPage = 10;
 
 /*** 
    Create the `showPage` function to hide all of the items in the 
@@ -34,8 +34,27 @@ FSJS project 2 - List Filter and Pagination
        that will be passed into the parens later when you call or 
        "invoke" the function 
 ***/
-
-
+// function to show each page, passing in array of total students and page number to show
+const showPage = (students, pageNumber) => {
+    
+    // set the starting index for first student on page
+    const startIndex = (pageNumber * numberPerPage) - numberPerPage;
+    // set the ending index for the last student on page
+    const endIndex = (pageNumber * numberPerPage) - 1;
+    
+    // for each student in students
+    for (let i = 0; i < students.length; i++) {
+        // if the student's index is between the starting and ending indexes
+        if (i >= startIndex && i <= endIndex) {
+            // show that student
+            students[i].style.display = '';
+        // else if student's index is outside the starting and ending indexes
+        } else if (i < startIndex || i > endIndex) {
+            // hide that student
+            students[i].style.display = 'none';
+        }
+    }
+};
 
 
 /*** 
@@ -43,8 +62,38 @@ FSJS project 2 - List Filter and Pagination
    functionality to the pagination buttons.
 ***/
 
+const appendPageLinks = students => {
+    const numberOfStudents = students.length;
+    const numberOfPageLinks = Math.ceil(numberOfStudents / numberPerPage);
+    const pageDiv = document.querySelector('.page');
+    const paginationDiv = document.createElement('div');
+    paginationDiv.className = 'pagination';
+    const paginationUl = document.createElement('ul');
+    
+    for (let i = 1; i <= numberOfPageLinks; i++) {
+        
+        const paginationLi = document.createElement('li');
+        
+        const paginationA = document.createElement('a');
+        paginationA.attributes.href = '#';
+        paginationA.textContent = i;
+        paginationA.addEventListener('click', (event) => {
+            showPage(studentList, event.target.textContent);
+            event.target.className = 'active';
+        });
+        
+        paginationLi.appendChild(paginationA);
+        paginationUl.appendChild(paginationLi);
+    }
+    
+    paginationDiv.appendChild(paginationUl);
+    
+    showPage(students, 1);
+    
+    pageDiv.appendChild(paginationDiv);
+    
+};
 
-
-
+document.addEventListener('DOMContentLoaded', appendPageLinks(studentList));
 
 // Remember to delete the comments that came with this file, and replace them with your own code comments.

4f3e33e1ec3c0d9a6e12305c9d7180d7bba8fe03
Andrew Bruner <andrewbruner@gmail.com>
2020-03-26 11:06:00 -0400

    Add initializeActivityRegistration function

--- js/script.js
+++ js/script.js
@@ -33,10 +33,9 @@ const initializeRegistration = () => {
     const initializeTshirtInfo = () => {
         const designSelect = document.querySelector('#design');
         const colorSelect = document.querySelector('#color');
-        const punColorOptions = [colorSelect[0], colorSelect[1], colorSelect[2]];
-        const heartColorOptions = [colorSelect[3], colorSelect[4], colorSelect[5]];
-        
-        const handleDesignSelectChange = (event) => {
+        const handleDesignSelectChange = () => {
+            const punColorOptions = [colorSelect[0], colorSelect[1], colorSelect[2]];
+            const heartColorOptions = [colorSelect[3], colorSelect[4], colorSelect[5]];
             // if default design option
             if (designSelect.value === 'Select Theme') {
                 // hide color selection
@@ -62,12 +61,68 @@ const initializeRegistration = () => {
                 colorSelect.selectedIndex = '3';
             }
         };
-        
         setDisplay(colorSelect, 'none')
         designSelect.addEventListener('change', handleDesignSelectChange);
     };
     
-    const initializeActivityRegistration = () => {};
+    const initializeActivityRegistration = () => {
+        const activitiesFieldset = document.querySelector('.activities');
+        const activitiesInputs = document.querySelectorAll('.activities input');
+        let totalCost = 0;
+        const handleActivitiesInputChange = (event) => {
+            const input = event.target;
+            if (input === activitiesInputs[1]) {
+                if (input.checked) {
+                    activitiesInputs[3].disabled = true;
+                    activitiesInputs[3].parentElement.style.color = 'grey';
+                } else {
+                    activitiesInputs[3].disabled = false;
+                    activitiesInputs[3].parentElement.style.color = '';
+                }
+            } else if (input === activitiesInputs[3]) {
+                if (input.checked) {
+                    activitiesInputs[1].disabled = true;
+                    activitiesInputs[1].parentElement.style.color = 'grey';
+                } else {
+                    activitiesInputs[1].disabled = false;
+                    activitiesInputs[1].parentElement.style.color = '';
+                }
+            } else if (input === activitiesInputs[2]) {
+                if (input.checked) {
+                    activitiesInputs[4].disabled = true;
+                    activitiesInputs[4].parentElement.style.color = 'grey';
+                } else {
+                    activitiesInputs[4].disabled = false;
+                    activitiesInputs[4].parentElement.style.color = '';
+                }
+            } else if (input === activitiesInputs[4]) {
+                if (input.checked) {
+                    activitiesInputs[2].disabled = true;
+                    activitiesInputs[2].parentElement.style.color = 'grey';
+                } else {
+                    activitiesInputs[2].disabled = false;
+                    activitiesInputs[2].parentElement.style.color = '';
+                }
+            }
+            if (input.checked) {
+                totalCost += Number(input.dataset.cost);
+            } else {
+                totalCost -= Number(input.dataset.cost);
+            }
+            const totalCostLabel = document.createElement('label');
+            totalCostLabel.id = 'totalCost';
+            totalCostLabel.style.fontWeight = 'bold';
+            totalCostLabel.style.marginTop = '1.5em';
+            totalCostLabel.textContent = `Total Cost: $${totalCost}`;
+            if (activitiesFieldset.lastElementChild.id === 'totalCost') {
+                activitiesFieldset.lastElementChild.remove();
+            }
+            if (totalCost !== 0) {
+                activitiesFieldset.appendChild(totalCostLabel);
+            }
+        };
+        activitiesFieldset.addEventListener('change', handleActivitiesInputChange);
+    };
     
     const initializePaymentInfo = () => {};
     

89422a77a2e185035b242a906e1249bea07aee2a
Andrew Bruner <andrewbruner@gmail.com>
2020-03-30 16:17:50 -0400

    Add initial start button click event listener

--- js/app.js
+++ js/app.js
@@ -2,3 +2,10 @@
  * Project 4 - OOP Game App
  * app.js */
 
+const startButton = document.querySelector('#btn__reset');
+
+const game = new Game();
+
+startButton.addEventListener('click', () => {
+	game.startGame();
+});

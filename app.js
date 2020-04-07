1684132365f8d31bd11ce8478352d349ce9ed749
Andrew Bruner <andrewbruner@gmail.com>
2020-04-07 16:24:13 -0400

    set view engine to pug

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-// import express module
+// import express module and set up main app
 const express = require('express');
+const app = express();
 
 // import data.json file
 const data = require('./data.json');
 
 // import path module
 const path = require('path');
+
+// set 'view engine' to 'pug'
+app.set('view engine', 'pug');

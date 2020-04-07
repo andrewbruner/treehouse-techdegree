b111e3a8cf7c498066d0e7656ac5cfd7d33fac7c
Andrew Bruner <andrewbruner@gmail.com>
2020-04-07 16:18:15 -0400

    Add module dependencies

---
+++ app.js
@@ -0,0 +1,8 @@
+// import express module
+const express = require('express');
+
+// import data.json file
+const data = require('./data.json');
+
+// import path module
+const path = require('path');

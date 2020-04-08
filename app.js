64a526cb47bf75625018c6c01db86622b83443ac
Andrew Bruner <andrewbruner@gmail.com>
2020-04-08 11:51:05 -0400

    Add route templates

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// DEPENDENCIES
+
 // import express module and set up main app
 const express = require('express');
 const app = express();
@@ -8,8 +10,33 @@ const data = require('./data.json');
 // import path module
 const path = require('path');
 
+
+
+
+// MIDDLEWARE //
+
 // set 'view engine' to 'pug'
 app.set('view engine', 'pug');
 
 // set up static files
 app.use('/static', express.static('public'));
+
+
+
+
+// ROUTES //
+
+// index route
+app.get('/', (req, res) => {
+
+});
+
+// about route
+app.get('/about', (req, res) => {
+
+});
+
+// dynamic projects route
+app.get('/projects', (req, res) => {
+
+});

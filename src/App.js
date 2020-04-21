c57f6f2cbccba7cf4e2baa5d195c0941b159bdfd
Andrew Bruner <andrewbruner@gmail.com>
2020-04-21 13:25:29 -0400

    Add empty PhotoContainer component

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './css/index.css'
 
 import SearchForm from './components/SearchForm';
 import MainNav from './components/MainNav';
+import PhotoContainer from './components/PhotoContainer';
 
 const App = () => (
   <div className="container">
     <SearchForm />
     <MainNav />
+    <PhotoContainer />
   </div>
 );
 
---
+++ src/components/PhotoContainer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class PhotoContainer extends Component {
+  state = {
+    
+  }
+
+  render() {
+    return (
+      <div className="photo-container">
+        <h2>Results</h2>
+        <ul>
+
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default PhotoContainer;

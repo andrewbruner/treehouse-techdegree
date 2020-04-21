b63fc8980325b57c1d07144e6ee20ba2e0c4f987
Andrew Bruner <andrewbruner@gmail.com>
2020-04-21 12:25:23 -0400

    Build SearchForm component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import './css/index.css'
+
+import SearchForm from './components/SearchForm';
 
 const App = () => (
   <div className="container">
-
+    <SearchForm />
   </div>
 );
 

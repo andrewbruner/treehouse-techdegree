1d4628dce169e6f3021d1633ed928315ab3ffcdb
Andrew Bruner <andrewbruner@gmail.com>
2020-04-21 13:21:06 -0400

    Add MainNav component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import './css/index.css'
 
 import SearchForm from './components/SearchForm';
+import MainNav from './components/MainNav';
 
 const App = () => (
   <div className="container">
     <SearchForm />
+    <MainNav />
   </div>
 );
 

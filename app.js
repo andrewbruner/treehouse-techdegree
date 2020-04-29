a8753b7c91affcaab371505d212c35280ec2ebec
Andrew Bruner <andrewbruner@gmail.com>
2020-04-29 10:06:34 -0400

    Add Book model and validation

--- app.js
+++ app.js
@@ -5,6 +5,28 @@ const sequelize = new Sequelize({
   storage: 'library.db'
 });
 
+class Book extends Sequelize.model {}
+Book.init({
+  title: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: {
+        msg: 'Title field is required'
+      }
+    }
+  },
+  author: {
+    type: Sequelize.STRING,
+    validate: {
+      notEmpty: {
+        msg: 'Author field is required'
+      }
+    }
+  },
+  genre: Sequelize.STRING,
+  year: Sequelize.INTEGER
+}, { sequelize });
+
 // async IIFE
 (async () => {
   try {

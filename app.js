aa10d7e73570b3fdc311881c18f9538085caded1
Andrew Bruner <andrewbruner@gmail.com>
2020-04-29 15:01:11 -0400

    Condense Initialization IIFE

--- app.js
+++ app.js
@@ -1,27 +1,45 @@
+// Include Express Module
 const express = require('express');
+// Create Express Application
 const app = express();
 
+// Include Sequelize Module
 const Sequelize = require('sequelize');
-
+// Set Up Database Connection
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: 'library.db'
 });
 
+// Routes
+app.get('/', async (req, res) => {});
+app.get('/books', async (req, res) => {});
+app.get('/books/new', async (req, res) => {});
+app.post('/books/new', async (req, res) => {});
+app.get('/books/:id', async (req, res) => {});
+app.post('/books/:id', async (req, res) => {});
+app.post('/books/:id/delete', async (req, res) => {});
 
-
-// async IIFE
+// Initialization IIFE
 (async () => {
-  // Force sync all models
-  await sequelize.sync({ force: true })
-  
+  // Test Database Connection
   try {
-
+    await sequelize.authenticate();
+    console.log('Database connection has been established successfully.')
   } catch (error) {
-    console.error('Error connecting to the database: ', error);
+    console.error('Unable to connect to the database: ', error);
+  }
+  // Force-Sync All Database Models
+  try {
+    await sequelize.sync({ force: true });
+    console.log('Database synchronized successfully.');
+  } catch (error) {
+    console.error('Unable to synchronize the database: ', error);
+  }
+  // Initialize Server
+  try {
+    app.listen(3000, () => console.log('Server listening on port 3000...'));
+  } catch (error) {
+    console.error('Server connection error: ', error);
   }
 })();
-
-
-
-app.listen(3000, () => console.log('Server listening on port 3000...'));

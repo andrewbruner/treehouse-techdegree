3178ad7eb433b75039997c17f7fa99a5db741e7f
Andrew Bruner <andrewbruner@gmail.com>
2020-05-09 20:05:45 +0000

    Add json parser to create-user

--- routes/user/create-user.js
+++ routes/user/create-user.js
@@ -1,3 +1,7 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
 const createUser = (router, asyncHandler, User) => {
     router.post('/users', asyncHandler(async (req, res) => {
         console.log(req.body);

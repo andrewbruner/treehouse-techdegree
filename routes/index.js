3d37a1d8f5dfa93055d600b7703c6620bc69cfce
Andrew Bruner <andrewbruner@gmail.com>
2020-05-09 18:42:54 +0000

    Authenticate readUser route

--- routes/index.js
+++ routes/index.js
@@ -3,56 +3,60 @@ const router = express.Router();
 const bcryptjs = require('bcryptjs');
 // use code below to hash a new user's password
 // user.password = bcryptjs.hashSync(user.password);
-// const auth = require('basic-auth');
+const auth = require('basic-auth');
 const db = require('../models');
 const { User } = db.models;
 const { Course } = db.models;
 
 // authentication middleware
-// const authenticateUser = (req, res, next) => {
-//   let message = null;
+const authenticateUser = (req, res, next) => {
+   let message = null;
 
-//   // Parse the user's credentials from the Authorization header.
-//   const credentials = auth(req);
+  // Parse the user's credentials from the Authorization header.
+  const credentials = auth(req);
 
-//   // If the user's credentials are available...
-//   if (credentials) {
-//     // Attempt to retrieve the user from the data store by their username (i.e. the user's "key" from the Authorization header).
-//     const user = users.find(u => u.username === credentials.name);
+  // If the user's credentials are available...
+  if (credentials) {
+    // Attempt to retrieve the user from the database by their email address (i.e. the user's "key" from the Authorization header).
+    const user = User.findOne({
+        where: {
+            emailAddress: credentials.emailAddress
+        }
+    });
 
-//     // If a user was successfully retrieved from the data store...
-//     if (user) {
-//       // Use the bcryptjs npm package to compare the user's password (from the Authorization header) to the user's password that was retrieved from the data store.
-//       const authenticated = bcryptjs.compareSync(credentials.pass, user.password);
+    // If a user was successfully retrieved from the database...
+    if (user) {
+      // Use the bcryptjs npm package to compare the user's password (from the Authorization header) to the user's password that was retrieved from the database.
+      const authenticated = bcryptjs.compareSync(credentials.password, user.password);
 
-//       // If the passwords match...
-//       if (authenticated) {
-//         console.log(`Authentication successful for username: ${user.username}`);
+      // If the passwords match...
+      if (authenticated) {
+        console.log(`Authentication successful for email address: ${user.emailAddress}`);
 
-//         // Then store the retrieved user object on the request object so any middleware functions that follow this middleware function will have access to the user's information.
-//         req.currentUser = user;
-//       } else {
-//         message = `Authentication failure for username: ${user.username}`;
-//       }
-//     } else {
-//       message = `User not found for username: ${credentials.name}`;
-//     }
-//   } else {
-//     message = 'Auth header not found';
-//   }
+        // Then store the retrieved user object on the request object so any middleware functions that follow this middleware function will have access to the user's information.
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for email address: ${user.emailAddress}`;
+      }
+    } else {
+      message = `User not found for email address: ${credentials.emailAddress}`;
+    }
+  } else {
+    message = 'Auth header not found';
+  }
 
-//   // If user authentication failed...
-//   if (message) {
-//     console.warn(message);
+  // If user authentication failed...
+  if (message) {
+    console.warn(message);
 
-//     // Return a response with a 401 Unauthorized HTTP status code.
-//     res.status(401).json({ message: 'Access Denied' });
-//   } else {
-//     // Or if user authentication succeeded...
-//     // Call the next() method.
-//     next();
-//   }
-// };
+    // Return a response with a 401 Unauthorized HTTP status code.
+    res.status(401).json({ message: 'Access Denied' });
+  } else {
+    // Or if user authentication succeeded...
+    // Call the next() method.
+    next();
+  }
+};
 
 const asyncHandler = cb => (
     async (req, res, next) => {
@@ -64,7 +68,7 @@ const asyncHandler = cb => (
     }
 );
 
-readUser = require('./user/read-user')(router, asyncHandler, User);
+readUser = require('./user/read-user')(router, authenticateUser, asyncHandler, User);
 createUser = require('./user/create-user');
 readCourses = require('./course/read-courses');
 readCourse = require('./course/read-course');
--- routes/user/read-user.js
+++ routes/user/read-user.js
@@ -1,6 +1,10 @@
-const readUser = (router, asyncHandler, User) => {
-    router.get('/users', asyncHandler(async (req, res) => {
-        const users = await User.findAll();
+const readUser = (router, authenticateUser, asyncHandler, User) => {
+    router.get('/users', authenticateUser, asyncHandler(async (req, res) => {
+        const users = await User.findAll({
+            where: {
+                emailAddress: req.currentUser.emailAddress
+            }
+        });
         res.json(users);
     }));
 };

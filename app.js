648a64fd086b80d6a2f0d8ec6be6151c90fe3620
Andrew Bruner <andrewbruner@gmail.com>
2020-05-09 15:21:53 +0000

    Add pseudocode for authentication middleware

--- app.js
+++ app.js
@@ -18,7 +18,31 @@ app.unsubscribe(express.json());
 // setup morgan which gives us http request logging
 app.use(morgan('dev'));
 
-// TODO setup your api routes here
+// authentication middleware
+const authenticateUser = (req, res, next) => {
+  // Parse the user's credentials from the Authorization header.
+
+  // If the user's credentials are available...
+  // Attempt to retrieve the user from the data store
+  // by their username (i.e. the user's "key"
+  // from the Authorization header).
+
+  // If a user was successfully retrieved from the data store...
+  // Use the bcryptjs npm package to compare the user's password
+  // (from the Authorization header) to the user's password
+  // that was retrieved from the data store.
+
+  // If the passwords match...
+  // Then store the retrieved user object on the request object
+  // so any middleware functions that follow this middleware function
+  // will have access to the user's information.
+
+  // If user authentication failed...
+  // Return a response with a 401 Unauthorized HTTP status code.
+
+  // Or if user authentication succeeded...
+  // Call the next() method.
+};
 
 // setup a friendly greeting for the root route
 app.get('/', (req, res) => {

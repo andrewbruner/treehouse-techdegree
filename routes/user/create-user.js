cb2d113f7bf2d5faae2323e5d15b88b256652738
Andrew Bruner <andrewbruner@gmail.com>
2020-05-09 20:01:03 +0000

    Log req.body

--- routes/user/create-user.js
+++ routes/user/create-user.js
@@ -1,5 +1,6 @@
 const createUser = (router, asyncHandler, User) => {
     router.post('/users', asyncHandler(async (req, res) => {
+        console.log(req.body);
         await User.create(req.body);
         res.status(201).location('/');
     }));

5acf207c9242f11e658ab0c9c3bd3905ab2dcd03
Andrew Bruner <andrewbruner@gmail.com>
2020-05-09 16:22:57 +0000

    Add all route modules

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const express = require('express');
 const morgan = require('morgan');
 const routes = require('./routes');
 const db = require('./models');
-const bcryptjs = require('bcryptjs');
-// use code below to hash a new user's password
-// user.password = bcryptjs.hashSync(user.password);
-// const auth = require('basic-auth');
 
 // variable to enable global error logging
 const enableGlobalErrorLogging = process.env.ENABLE_GLOBAL_ERROR_LOGGING === 'true';
@@ -21,52 +17,6 @@ app.unsubscribe(express.json());
 
 // setup morgan which gives us http request logging
 app.use(morgan('dev'));
-
-// authentication middleware
-// const authenticateUser = (req, res, next) => {
-//   let message = null;
-
-//   // Parse the user's credentials from the Authorization header.
-//   const credentials = auth(req);
-
-//   // If the user's credentials are available...
-//   if (credentials) {
-//     // Attempt to retrieve the user from the data store by their username (i.e. the user's "key" from the Authorization header).
-//     const user = users.find(u => u.username === credentials.name);
-
-//     // If a user was successfully retrieved from the data store...
-//     if (user) {
-//       // Use the bcryptjs npm package to compare the user's password (from the Authorization header) to the user's password that was retrieved from the data store.
-//       const authenticated = bcryptjs.compareSync(credentials.pass, user.password);
-
-//       // If the passwords match...
-//       if (authenticated) {
-//         console.log(`Authentication successful for username: ${user.username}`);
-
-//         // Then store the retrieved user object on the request object so any middleware functions that follow this middleware function will have access to the user's information.
-//         req.currentUser = user;
-//       } else {
-//         message = `Authentication failure for username: ${user.username}`;
-//       }
-//     } else {
-//       message = `User not found for username: ${credentials.name}`;
-//     }
-//   } else {
-//     message = 'Auth header not found';
-//   }
-
-//   // If user authentication failed...
-//   if (message) {
-//     console.warn(message);
-
-//     // Return a response with a 401 Unauthorized HTTP status code.
-//     res.status(401).json({ message: 'Access Denied' });
-//   } else {
-//     // Or if user authentication succeeded...
-//     // Call the next() method.
-//     next();
-//   }
-// };
 
 // setup a friendly greeting for the root route
 app.get('/', (req, res) => {

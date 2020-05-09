20c8f03ac54cccbb4c9b8fdcad2fe870a8e6dc41
Andrew Bruner <andrewbruner@gmail.com>
2020-05-09 20:31:18 +0000

    Test

--- routes/user/create-user.js
+++ routes/user/create-user.js
@@ -1,7 +1,3 @@
-// const express = require('express');
-// const app = express();
-// app.use(express.json());
-
 const createUser = (router, asyncHandler, User) => {
     router.post('/users', asyncHandler(async (req, res) => {
         console.dir(req.body);

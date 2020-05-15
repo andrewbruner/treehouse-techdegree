7b7be7fc1175e9bb452787741f292ab8e217879c
Andrew Bruner <andrewbruner@gmail.com>
2020-05-15 09:50:56 -0400

    Refactor conditionals to allow Sequelize validation

--- routes/course/update-course.js
+++ routes/course/update-course.js
@@ -15,16 +15,12 @@ const updateCourse = (router, authenticateUser, asyncHandler, Course) => {
 		throw error;
             }
 	    // test if body.title and body.description are present
-	    // (Sequelize validation would allow an empty request body on this PUT route otherwise)
+	    // change each to an empty string to allow Sequelize validation to catch it
 		if (!req.body.title) {
-		    const error = new Error('Please enter a valid title.');
-		    error.name = 'NullError';
-		    throw error;
+		    req.body.title = '';
 		}
 		if (!req.body.description) {
-		    const error = new Error('Please enter a valid description.');
-		    error.name = 'NullError';
-		    throw error;
+		    req.body.description = '';
 		}
             // update current course
             await Course.update(req.body, {
@@ -40,12 +36,6 @@ const updateCourse = (router, authenticateUser, asyncHandler, Course) => {
 	      message.push(err.message);
               console.error('Access Error: ', message);
 	      return res.status(403).json({ message: message });
-	    }
-	    // if null error, log specific message
-	    if (err.name === 'NullError') {
-	      message.push(err.message);
-	      console.error('Validation Error: ', message);
-	      return res.status(400).json({ message: message });
 	    }
             // on other error(s), log validation message(s)
             err.errors.forEach(error => message.push(error.message));

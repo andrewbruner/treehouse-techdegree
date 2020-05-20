07bc025d89e02598b3131facd37e919c08116590
Andrew Bruner <andrewbruner@gmail.com>
2020-05-20 18:58:51 +0000

    Refactor whitespace

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 
 class UserSignIn extends Component {
-    render() {
+	render() {
         return (
         	<div className="bounds">
         		<div className="grid-33 centered signin">
           			<h1>Sign In</h1>
           			<div>
-            				<form>
-              					<div><input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" value="" /></div>
-              					<div><input id="password" name="password" type="password" className="" placeholder="Password" value="" /></div>
-              					<div className="grid-100 pad-bottom"><button className="button" type="submit">Sign In</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='/';">Cancel</button></div>
-            				</form>
+						<form>
+							<div><input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" value="" /></div>
+							<div><input id="password" name="password" type="password" className="" placeholder="Password" value="" /></div>
+							<div className="grid-100 pad-bottom"><button className="button" type="submit">Sign In</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='/';">Cancel</button></div>
+						</form>
           			</div>
           			<p>&nbsp;</p>
           			<p>Don't have a user account? <a href="/signup">Click here</a> to sign up!</p>
         		</div>
-		</div>
+			</div>
         )
     }
 }

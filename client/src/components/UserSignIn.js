7bed32196bbeb328ca7dee4d2461750162e5c488
Andrew Bruner <andrewbruner@gmail.com>
2020-05-20 14:50:32 -0400

    Update UserSignIn component

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -3,7 +3,20 @@ import React, { Component } from 'react';
 class UserSignIn extends Component {
     render() {
         return (
-            <h1>This is the UserSignIn</h1>
+        	<div className="bounds">
+        		<div className="grid-33 centered signin">
+          			<h1>Sign In</h1>
+          			<div>
+            				<form>
+              					<div><input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" value="" /></div>
+              					<div><input id="password" name="password" type="password" className="" placeholder="Password" value="" /></div>
+              					<div className="grid-100 pad-bottom"><button className="button" type="submit">Sign In</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='/';">Cancel</button></div>
+            				</form>
+          			</div>
+          			<p>&nbsp;</p>
+          			<p>Don't have a user account? <a href="/signup">Click here</a> to sign up!</p>
+        		</div>
+		</div>
         )
     }
 }

48173c08979d947c56c96d44770310132222b96c
Andrew Bruner <andrewbruner@gmail.com>
2020-05-21 18:11:20 +0000

    Attempt handleDelete()

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,10 @@ import UserSignOut from './components/UserSignOut';
   const host = 'http://penguin.linux.test:5000';
   
 class App extends Component {
+  handleDelete = async id => {
+    await fetch(`${host}/api/courses/${id}`, { method: 'delete' })
+  }
+
   render() {
     return (
       <AuthenticationContext.Provider value={false}>
@@ -33,7 +37,7 @@ class App extends Component {
             <Route exact path="/" render={props => <Courses host={host} />} />
             <Route path="/courses/create" render={props => <CreateCourse />} />
             <Route path="/courses/:id/update" render={props => <UpdateCourse host={host} id={props.match.params.id} />} />
-            <Route path="/courses/:id" render={props => <CourseDetail host={host} id={props.match.params.id} />} />
+            <Route path="/courses/:id" render={props => <CourseDetail host={host} id={props.match.params.id} handleDelete={this.handleDelete} />} />
             <Route path="/signin" render={props => <UserSignIn />} />
             <Route path="/signup" render={props => <UserSignUp />} />
             <Route path="/signout" render={props => <UserSignOut />} />
--- client/src/components/CourseDetail.js
+++ client/src/components/CourseDetail.js
@@ -2,59 +2,64 @@ import React, { Component } from 'react';
 
 class CourseDetail extends Component {
     state = {
-        courseDetail: { user: { } }
+      courseDetail: { user: { } }
     }
 
     readCourseDetail = () => {
-        fetch(`${this.props.host}/api/courses/${this.props.id}`)
-            .then(response => response.json())
-            .then(data => this.setState({ courseDetail: data }))
-            .catch(err => console.error('Error: ', err))
+      fetch(`${this.props.host}/api/courses/${this.props.id}`)
+        .then(response => response.json())
+        .then(data => this.setState({ courseDetail: data }))
+        .catch(err => console.error('Error: ', err))
     }
 
     componentDidMount() {
-        this.readCourseDetail();
+      this.readCourseDetail();
     }
 
     render() {
-        return (
-            <div>
-        <div className="actions--bar">
-          <div className="bounds">
-            <div className="grid-100"><span><a className="button" href={`/courses/${this.state.courseDetail.id}/update`}>Update Course</a><a className="button" href="#">Delete Course</a></span><a
-                className="button button-secondary" href="/">Return to List</a></div>
+      return (
+        <div>
+          <div className="actions--bar">
+            <div className="bounds">
+              <div className="grid-100">
+                <span>
+                  <a className="button" href={`/courses/${this.state.courseDetail.id}/update`}>Update Course</a>
+                  <a className="button" href="/" onClick={() => this.props.handleDelete(this.state.courseDetail.id)}>Delete Course</a>
+                </span>
+                <a className="button button-secondary" href="/">Return to List</a>
+              </div>
+            </div>
+          </div>
+          <div className="bounds course--detail">
+            <div className="grid-66">
+              <div className="course--header">
+                <h4 className="course--label">Course</h4>
+                <h3 className="course--title">{this.state.courseDetail.title}</h3>
+                <p>{this.state.courseDetail.user.firstName} {this.state.courseDetail.user.lastName}</p>
+              </div>
+              <div className="course--description">
+                <p>{this.state.courseDetail.description}</p>
+              </div>
+            </div>
+            <div className="grid-25 grid-right">
+              <div className="course--stats">
+                <ul className="course--stats--list">
+                  <li className="course--stats--list--item">
+                    <h4>Estimated Time</h4>
+                    <h3>{this.state.courseDetail.estimatedTime}</h3>
+                  </li>
+                  <li className="course--stats--list--item">
+                    <h4>Materials Needed</h4>
+                    <ul>
+                      <li>{this.state.courseDetail.materialsNeeded}</li>
+                    </ul>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="bounds course--detail">
-          <div className="grid-66">
-            <div className="course--header">
-              <h4 className="course--label">Course</h4>
-        <h3 className="course--title">{this.state.courseDetail.title}</h3>
-              <p>{this.state.courseDetail.user.firstName} {this.state.courseDetail.user.lastName}</p>
-            </div>
-            <div className="course--description">
-            <p>{this.state.courseDetail.description}</p>
-            </div>
-          </div>
-          <div className="grid-25 grid-right">
-            <div className="course--stats">
-              <ul className="course--stats--list">
-                <li className="course--stats--list--item">
-                  <h4>Estimated Time</h4>
-                  <h3>{this.state.courseDetail.estimatedTime}</h3>
-                </li>
-                <li className="course--stats--list--item">
-                  <h4>Materials Needed</h4>
-                  <ul>
-                    <li>{this.state.courseDetail.materialsNeeded}</li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-        );
+      );
     }
 }
 

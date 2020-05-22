7e59b8d243e333bcbfddb0fda9912164503e4196
Andrew Bruner <andrewbruner@gmail.com>
2020-05-22 22:37:57 +0000

    Update the cancel button

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -8,9 +8,16 @@ class UserSignIn extends Component {
           			<h1>Sign In</h1>
           			<div>
 						<form>
-							<div><input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" value="" /></div>
-							<div><input id="password" name="password" type="password" className="" placeholder="Password" value="" /></div>
-							<div className="grid-100 pad-bottom"><button className="button" type="submit">Sign In</button><button className="button button-secondary" onclick="event.preventDefault(); location.href='/';">Cancel</button></div>
+							<div>
+								<input id="emailAddress" name="emailAddress" type="text" className="" placeholder="Email Address" value="" />
+							</div>
+							<div>
+								<input id="password" name="password" type="password" className="" placeholder="Password" value="" />
+							</div>
+							<div className="grid-100 pad-bottom">
+								<button className="button" type="submit">Sign In</button>
+								<button className="button button-secondary" onClick={(e) => e.preventDefault(), location.href='/'}>Cancel</button>
+							</div>
 						</form>
           			</div>
           			<p>&nbsp;</p>

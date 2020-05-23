a32b3ba47a7fb16e71037b010de41fe272b0e895
Andrew Bruner <andrewbruner@gmail.com>
2020-05-23 12:26:08 -0400

    Refactor Header for WithContext

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,19 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Header = props => {
-  let authenticationNavigation = '';
-  props.authenticated
+const Header = ({ context }) => {
   ? authenticationNavigation = <nav><span>Welcome Joe Smith!</span><a className="signout" href="/signout">Sign Out</a></nav>
   : authenticationNavigation = <nav><a className="signup" href="/signup">Sign Up</a><a className="signin" href="/signin">Sign In</a></nav>
 
-  return (
-    <div className="header">
-      <div className="bounds">
-        <h1 className="header--logo">Courses</h1>
-        {authenticationNavigation}
-      </div>
-    </div>
-  );
+	const authUser = context.authenticatedUser
+	return (
+		<div className="header">
+			<div className="bounds">
+				<h1 className="header--logo">Courses</h1>
+				<nav>
+					{authUser ? (
+						<React.Fragment>
+							<span>Welcome {authUser.firstName} {authUser.lastName}!</span>
+							<Link className="signout" to="/signout">Sign Out</Link>
+						</React.Fragment>
+					) : (
+						<React.Fragment>
+							<Link className="signup" to="/signup">Sign Up</Link>
+							<Link className="signin" to="/signin">Sign In</Link>
+						</React.Fragment>
+					)}
+				</nav>
+			</div>
+		</div>
+	);
 }
 
 export default Header;

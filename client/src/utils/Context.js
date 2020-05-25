cb748e6e06c0ee50065060325dfe7feb57470936
Andrew Bruner <andrewbruner@gmail.com>
2020-05-25 15:13:31 +0000

    Refactor

--- client/src/utils/Context.js
+++ client/src/utils/Context.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import the Data.js file containing the helper class
 import Data from './Data';
 
 const Context = React.createContext(); 
@@ -8,42 +7,34 @@ export class Provider extends Component {
 
   constructor() {
     super();
-    // initialize a new instance of the Data class inside the constructor() method
-    // assign it to a data property (this.data)
     this.data = new Data();
   }
 
   render() {
 
-    // create a value object to provide the utility methods of the Data class
     const value = {
       data: this.data,
       actions: {
-        // pass signIn method to the Provider
         signIn: this.signIn,
       }
     };
 
     return (
-      // pass the Provider a value prop
       <Context.Provider value={value}>
         {this.props.children}
       </Context.Provider>  
     );
   }
-
   
   signIn = async (username, password) => {
-    // initialize user as the returned object of getUser()
     const user = await this.data.getUser(username, password);
-    // return that user
     return user;
   }
 
   signOut = () => {
 
   }
-}
+};
 
 export const Consumer = Context.Consumer;
 
@@ -61,4 +52,4 @@ export default function withContext(Component) {
       </Context.Consumer>
     );
   }
-}
+};

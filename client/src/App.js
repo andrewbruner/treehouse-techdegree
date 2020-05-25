8ded3ece128748041db6719f9514286c8b036762
Andrew Bruner <andrewbruner@gmail.com>
2020-05-25 17:52:51 +0000

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -8,41 +8,42 @@ import './styles/global.css';
 
 // Components
 import Header from './components/Header';
-import Courses from './components/Courses';
-import CreateCourse from './components/CreateCourse';
-import CourseDetail from './components/CourseDetail';
-import UpdateCourse from './components/UpdateCourse';
+// import Courses from './components/Courses';
+// import CreateCourse from './components/CreateCourse';
+// import CourseDetail from './components/CourseDetail';
+// import UpdateCourse from './components/UpdateCourse';
 import UserSignUp from './components/UserSignUp';
-import UserSignIn from './components/UserSignIn';
-import UserSignOut from './components/UserSignOut';
+// import UserSignIn from './components/UserSignIn';
+// import UserSignOut from './components/UserSignOut';
 
-// import the withContext function from Context.js
+// Context
 import withContext from './utils/Context';
 
+// Components with Context
 const HeaderWithContext = withContext(Header);
 const UserSignUpWithContext = withContext(UserSignUp);
-const UserSignInWithContext = withContext(UserSignIn);
+// const UserSignInWithContext = withContext(UserSignIn);
 
   
-class App extends Component {
+export default class App extends Component {
 
   render() {
     return (
+      <div>
+        <HeaderWithContext />
+        <hr />
         <Router>
-          <HeaderWithContext />
-          <hr />
           <Switch>
-            <Route exact path="/" component={Courses} />
-            <Route path="/courses/create" component={CreateCourse} />
-            <Route path="/courses/:id" component={CourseDetail} />
-            <Route path="/courses/:id/update" component={UpdateCourse} />
+            {/* <Route exact path="/" component={Courses} /> */}
+            {/* <Route path="/courses/create" component={CreateCourse} /> */}
+            {/* <Route path="/courses/:id" component={CourseDetail} /> */}
+            {/* <Route path="/courses/:id/update" component={UpdateCourse} /> */}
             <Route path="/signup" component={UserSignUpWithContext} />
-            <Route path="/signin" component={UserSignInWithContext} />
-            <Route path="/signout" component={UserSignOut} />
+            {/* <Route path="/signin" component={UserSignInWithContext} /> */}
+            {/* <Route path="/signout" component={UserSignOut} /> */}
           </Switch>
         </Router>
+      </div>
     )
   }
-}
-
-export default App;
+};

f86cbcee30f2e1b9eda865c99adba7baaf7541d9
Andrew Bruner <andrewbruner@gmail.com>
2020-05-26 00:32:39 +0000

    Add Provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { Provider } from './utils/Context'
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  // <React.StrictMode>
+  <Provider>
     <App />
-  </React.StrictMode>,
+  </Provider>
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 

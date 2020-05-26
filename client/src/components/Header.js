f7d44ddbab0d2dec75b73b2af82b487ee4e738e4
Andrew Bruner <andrewbruner@gmail.com>
2020-05-26 01:18:50 +0000

    Change Header to functional component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default class Header extends React.PureComponent {
-  render() {
-    return (
-      <div className="header">
-        <div className="bounds">
-          <h1 className="header--logo">Courses</h1>
-          <nav>
-            {authUser ? (
-              <React.Fragment>
-                <span>Welcome {authUser.firstName} {authUser.lastName}!</span>
-                <Link className="signout" to="/"></Link>
-              </React.Fragment>
-            ) : (
+export default props => {
+  const { context } = props;
+  const authUser = context.authenticatedUser;
+  return (
+    <div className="header">
+      <div className="bounds">
+        <h1 className="header--logo">Courses</h1>
+        <nav>
+          {authUser ? (
             <React.Fragment>
-              <Link className="signup" to="/signup">Sign Up</Link>
-              <Link className="signin" to="/signin">Sign In</Link>
+              <span>Welcome {authUser.firstName} {authUser.lastName}!</span>
+              <Link className="signout" to="/"></Link>
             </React.Fragment>
-            )}
-          </nav>
-        </div>
+          ) : (
+          <React.Fragment>
+            <Link className="signup" to="/signup">Sign Up</Link>
+            <Link className="signin" to="/signin">Sign In</Link>
+          </React.Fragment>
+          )}
+        </nav>
       </div>
-    );
-  }
+    </div>
+  );
 };

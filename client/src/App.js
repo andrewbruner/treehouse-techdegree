09c288efa1b8435562d4d5fc10ab16606805ee5b
Andrew Bruner <andrewbruner@gmail.com>
2020-05-28 20:21:27 +0000

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -7,25 +7,33 @@ import './styles/global.css';
 
 // Components
 import Header from './components/Header';
+
 import UserSignUp from './components/UserSignUp';
 import UserSignIn from './components/UserSignIn';
 import UserSignOut from './components/UserSignOut';
+
 import Courses from './components/Courses';
 import CreateCourse from './components/CreateCourse';
 import CourseDetail from './components/CourseDetail';
 import UpdateCourse from './components/UpdateCourse';
 
+import NotFound from './components/NotFound';
+import Forbidden from './components/Forbidden';
+import UnHandledError from './components/UnhandledError';
 
-// Context/Private Route
 import withContext from './utils/Context';
 import CourseDetail from './components/CourseDetail';
 import PrivateRoute from './utils/PrivateRoute';
 
+
 // Components with Context
 const HeaderWithContext = withContext(Header);
+
 const UserSignUpWithContext = withContext(UserSignUp);
 const UserSignInWithContext = withContext(UserSignIn);
 const UserSignOutWithContext = withContext(UserSignOut);
+
+const CoursesWithContext = withContext(Courses);
 const CreateCourseWithContext = withContext(CreateCourse);
 const CourseDetailWithContext = withContext(CourseDetail);
 const UpdateCourseWithContext = withContext(UpdateCourse);
@@ -39,13 +47,14 @@ export default class App extends Component {
           <HeaderWithContext />
           <hr />
           <Switch>
-            <Route exact path="/" component={Courses} />
+            <Route exact path="/" component={CoursesWithContext} />
             <Route path="/signup" component={UserSignUpWithContext} />
             <Route path="/signin" component={UserSignInWithContext} />
             <Route path="/signout" component={UserSignOutWithContext} />
             <Route path="/courses/create" component={CreateCourseWithContext} />
             <Route path="/courses/:id" component={CourseDetailWithContext} />
             <Route path="/courses/:id/update" component={UpdateCourseWithContext} />
+            <Route component={NotFound} />
           </Switch>
         </div>
       </Router>

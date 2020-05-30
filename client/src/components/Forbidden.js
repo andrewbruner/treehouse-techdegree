e7041c1680432c14e1d1bcaaed0f0886ec183b72
Andrew Bruner <andrewbruner@gmail.com>
2020-05-30 19:17:46 +0000

    Fix syntax

--- client/src/components/Forbidden.js
+++ client/src/components/Forbidden.js
@@ -2,9 +2,11 @@ import React, { Component } from 'react';
 
 export default class Forbidden extends Component {
     render() {
-        <div className="bounds">
-            <h1>Forbidden</h1>
-            <p>Oh oh! You can't access this page.</p>
-        </div>
+        return (
+            <div className="bounds">
+                <h1>Forbidden</h1>
+                <p>Oh oh! You can't access this page.</p>
+            </div>
+        );
     }
 };

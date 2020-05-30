deda58654ee403e931fe5e51f4ad7a37a96c9958
Andrew Bruner <andrewbruner@gmail.com>
2020-05-30 19:16:31 +0000

    Update NotFound and Forbidden components

--- client/src/components/Forbidden.js
+++ client/src/components/Forbidden.js
@@ -1,3 +1,10 @@
 import React, { Component } from 'react';
 
-export default class Forbidden extends Component {};
+export default class Forbidden extends Component {
+    render() {
+        <div className="bounds">
+            <h1>Forbidden</h1>
+            <p>Oh oh! You can't access this page.</p>
+        </div>
+    }
+};
--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,3 +1,10 @@
 import React, { Component } from 'react';
 
-export default class NotFound extends Component {};
+export default class NotFound extends Component {
+    render() {
+        <div className="bounds">
+            <h1>Not Found</h1>
+            <p>Sorry! We couldn't find the page you're looking for.</p>
+        </div>
+    }
+};

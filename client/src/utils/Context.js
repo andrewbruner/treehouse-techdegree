495798d803813e8e909dc353490a2f8c5db59619
Andrew Bruner <andrewbruner@gmail.com>
2020-05-30 14:55:46 +0000

    Move actions outside of render( )

--- client/src/utils/Context.js
+++ client/src/utils/Context.js
@@ -36,100 +36,100 @@ export class Provider extends Component {
       },
     };
 
-    // Sign Up / Create User
-    const signUp = async (user, confirmPassword) => {
-
-      // Passwords Match
-      if (user.password === confirmPassword) {
-
-        // Fetch API
-        await this.data.api('/users', 'POST', user)
-
-          .then(response => {
-
-            // Response: Created
-            if (response.status === 201) {
-              return [];
-
-            // Response: Bad Request
-            } else if (response.status === 400) {
-              return response.json()
-                .then(data => [data.message]);
-
-            // Other Response
-            } else {
-              throw new Error();
-            }
-          });
-
-      // Passwords Do Not Match
-      } else {
-        const error = { message: 'Passwords do not match' };
-        return error.message;
-      }
-    };    
-
-    // Sign In / Get User
-    const signIn = async (emailAddress, password) => {
-
-      // Fetch API
-      await this.data.api(`/users`, 'GET', null, true, { emailAddress, password })
-
-        .then(response => {
-
-          // Response: OK
-          if (response.status === 200) {
-            return response.json()
-              .then(data => {
-                const user = { firstName: data.firstName, lastName: data.lastName, emailAddress: data.emailAddress, };
-                this.setState(() => ({ authenticatedUser: user }));
-                Cookies.set('authenticatedUser', JSON.stringify(user));
-                return user;
-              });
-
-          // Response: Unauthorized
-          } else if (response.status === 401) {
-            return response.json()
-              .then(error => error.message);
-
-          // Other Response
-          } else {
-            throw new Error();
-          }
-
-        });
-    };
-  
-    // Sign Out
-    const signOut = () => {
-      this.setState(() => ({ authenticatedUser: null }));
-      Cookies.remove('authenticatedUser');
-    };
-
-    // Get Courses
-    const getCourses = () => {
-      this.data.api('/courses', 'GET')
-        .then(response => {
-
-          // Response: OK
-          if (response.status === 200) {
-            return response.json()
-              .then(courses => courses);
-
-          // Other Response
-          } else {
-            throw new Error();
-          }
-        });
-    };
-  
     return (
       <Context.Provider value={value}>
         {this.props.children}
       </Context.Provider>  
     );
   }
-};
+
+  // Sign Up / Create User
+  signUp = async (user, confirmPassword) => {
+
+    // Passwords Match
+    if (user.password === confirmPassword) {
+
+      // Fetch API
+      await this.data.api('/users', 'POST', user)
+
+        .then(response => {
+
+          // Response: Created
+          if (response.status === 201) {
+            return [];
+
+          // Response: Bad Request
+          } else if (response.status === 400) {
+            return response.json()
+              .then(data => [data.message]);
+
+          // Other Response
+          } else {
+            throw new Error();
+          }
+        });
+
+    // Passwords Do Not Match
+    } else {
+      const error = { message: 'Passwords do not match' };
+      return error.message;
+    }
+  }
+
+  // Sign In / Get User
+  signIn = async (emailAddress, password) => {
+
+    // Fetch API
+    await this.data.api(`/users`, 'GET', null, true, { emailAddress, password })
+
+      .then(response => {
+
+        // Response: OK
+        if (response.status === 200) {
+          return response.json()
+            .then(data => {
+              const user = { firstName: data.firstName, lastName: data.lastName, emailAddress: data.emailAddress, };
+              this.setState(() => ({ authenticatedUser: user }));
+              Cookies.set('authenticatedUser', JSON.stringify(user));
+              return user;
+            });
+
+        // Response: Unauthorized
+        } else if (response.status === 401) {
+          return response.json()
+            .then(error => error.message);
+
+        // Other Response
+        } else {
+          throw new Error();
+        }
+
+      });
+  }
+
+  // Sign Out
+  signOut = () => {
+    this.setState(() => ({ authenticatedUser: null }));
+    Cookies.remove('authenticatedUser');
+  }
+
+  // Get Courses
+  getCourses = () => {
+    this.data.api('/courses', 'GET')
+      .then(response => {
+
+        // Response: OK
+        if (response.status === 200) {
+          return response.json()
+            .then(courses => courses);
+
+        // Other Response
+        } else {
+          throw new Error();
+        }
+      });
+  }
+}
 
 // export Consumer component
 export const Consumer = Context.Consumer;

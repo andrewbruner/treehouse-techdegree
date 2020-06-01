3c1bddf19caa69d7b447ab54473ff1e1e8285d15
Andrew Bruner <andrewbruner@gmail.com>
2020-06-01 17:53:53 +0000

    Reconfig apiBaseUrl

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -7,5 +7,5 @@ export default {
   // apiBaseUrl: 'http://penguin.linux.test:5000/api',
   
   // VSCodespaces
-  apiBaseUrl: 'https://dd099cd27a76caa053f205e90969a39c8e06-5000.app.online.visualstudio.com/api',
+  apiBaseUrl: 'https://a8a6cd7910515cbd2b9468e573cc8c3e103d-5000.app.online.visualstudio.com/api',
 };

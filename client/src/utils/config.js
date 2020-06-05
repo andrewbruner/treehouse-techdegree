2e41e4008b722722f91a1c6858fc7aadb0955ca8
Andrew Bruner <andrewbruner@gmail.com>
2020-06-05 18:09:59 +0000

    Set to default port configuration

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -1,11 +1,11 @@
 export default {
-  
+
   // Default
   apiBaseUrl: 'http://localhost:5000/api',
-  
+
   // ChromeOS
   // apiBaseUrl: 'http://penguin.linux.test:5000/api',
-  
+
   // VSCodespaces
-  // apiBaseUrl: 'https://a8a6cd7910515cbd2b9468e573cc8c3e103d-5000.app.online.visualstudio.com/api',
+  // apiBaseUrl: '[url for forwarded port (5000)]/api',
 };

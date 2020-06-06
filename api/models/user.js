72069a0d2f5d13e0c00b16017e7828a6eb7e4c38
Andrew Bruner <andrewbruner@gmail.com>
2020-06-06 19:34:26 +0000

    Edit email validation message

--- api/models/user.js
+++ api/models/user.js
@@ -15,59 +15,59 @@ module.exports = sequelize => {
       allowNull: false,
       validate: {
         notNull: {
-	  msg: 'Please enter a valid first name.'
-	},
+	        msg: 'Please enter a valid first name.',
+	      },
         notEmpty: {
-          msg: 'Please enter a valid first name.'
-	}
-      }
+          msg: 'Please enter a valid first name.',
+	      },
+      },
     },
     lastName: {
       type: Sequelize.STRING,
       allowNull: false,
       validate: {
         notNull: {
-	  msg: 'Please enter a valid last name.'
-	},
+	        msg: 'Please enter a valid last name.',
+	      },
         notEmpty: {
-          msg: 'Please enter a valid last name.'
-	}
-      }
+          msg: 'Please enter a valid last name.',
+	      },
+      },
     },
     emailAddress: {
       type: Sequelize.STRING,
       allowNull: false,
       validate: {
         notNull: {
-	  msg: 'Please enter a valid email address.'
-	},
+	        msg: 'Please enter a valid email address.',
+	      },
         notEmpty: {
-          msg: 'Please enter a valid email address.'
-	},
-	isEmail: {
-	  msg: 'Please enter a valid email address.'
-	},
-	isUnique: async email => {
-	  const matchingUser = await User.findOne({ attributes: ['emailAddress'], where: { emailAddress: email } });
+          msg: 'Please enter a valid email address.',
+	      },
+	      isEmail: {
+	        msg: 'Email address must be a valid format.',
+	      },
+	      isUnique: async email => {
+	        const matchingUser = await User.findOne({ attributes: ['emailAddress'], where: { emailAddress: email } });
           if (matchingUser) {
-	    throw new Error(`Sorry, email address ${matchingUser.emailAddress} is already in use.`);
-	  }
-	}
-      }
+	          throw new Error(`Sorry, email address ${matchingUser.emailAddress} is already in use.`);
+	        }
+	      },
+      },
     },
     password: {
       type: Sequelize.STRING,
       allowNull: false,
       validate: {
         notNull: {
-	  msg: 'Please enter a valid password.'
-	},
-	notEmpty: {
-          msg: 'Please enter a valid password.'
-	}
-      }
-    }
- }, { sequelize, modelName: 'user' });
+	        msg: 'Please enter a valid password.',
+	      },
+	      notEmpty: {
+          msg: 'Please enter a valid password.',
+	      },
+      },
+    },
+  }, { sequelize, modelName: 'user' });
 
   User.associate = models => {
     User.hasMany(models.Course, { foreignKey: 'userId' });
